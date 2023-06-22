5e9b4b5f91bdde02bbaa29ee61e2ff4151d8df8c
HARI-257 <88844719+HARI-257@users.noreply.github.com>
2023-06-22 09:59:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+var items = []
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/',(req,res) =>{
+
+    var today = new Date()
+    var options = {
+        weekday:'long',
+        day:'numeric',
+        month:'long'
+    }
+    var day = today.toLocaleDateString('en-US',options)
+    res.render('list',{currentDate: day,Todos:items})
+})
+
+app.post('/',(req,res) =>{
+
+    var item = req.body.todo
+    items.push(item)
+    res.redirect('/')
+})
+
+app.listen(3000,()=>{
+    console.log('3000 listening');
+})

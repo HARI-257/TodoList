1a34c75c13337e065896fa1a4dd056b66a74ae14
HARI-257 <88844719+HARI-257@users.noreply.github.com>
2023-06-25 10:52:50 +0530

    add ejs layout and /work /about

--- app.js
+++ app.js
@@ -3,27 +3,47 @@ const bodyParser = require('body-parser')
 
 
 const app = express()
-var items = []
+let items = []
+let workList = []
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.get('/',(req,res) =>{
 
-    var today = new Date()
-    var options = {
+    let today = new Date()
+    let options = {
         weekday:'long',
         day:'numeric',
         month:'long'
     }
-    var day = today.toLocaleDateString('en-US',options)
-    res.render('list',{currentDate: day,Todos:items})
+    let day = today.toLocaleDateString('en-US',options)
+    res.render('list',{listTitle: day,Todos:items})
 })
 
 app.post('/',(req,res) =>{
+    let item = req.body.todo
+    if(req.body.list === 'Work'){
+        workList.push(item)
+        res.redirect('/work')
+    }else{
+        items.push(item)
+        res.redirect('/')
+    }
+    
+})
 
-    var item = req.body.todo
-    items.push(item)
-    res.redirect('/')
+app.get('/work',(req,res) => {
+    res.render('list',{listTitle: "Work List", Todos: workList})
+})
+
+app.post('/work',(req,res) => {
+    let item = req.body.todo
+    workList.push(item)
+    res.redirect('/work')
+})
+
+app.get('/about',(req,res) => {
+    res.render('about')
 })
 
 app.listen(process.env.PORT || 3000,()=>{
